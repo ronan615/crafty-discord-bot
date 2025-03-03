c434411c1e02fa8c7789e4ee7ce5c0a762f8ddfd
ronan chan <85199004+ronan615@users.noreply.github.com>
2025-03-03 13:27:35 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+DISCORD_TOKEN = "" #your discord bot token
+CRAFTY_API_KEY = "" #in crafty,generate a api key
+MINECRAFT_SERVER_ID = "" #in crafty config find the server id
+CRAFTY_API_URL = "" #your crafty server management link, make sure you use port fowarding 
+CERT_PATH = "path_to_your/public_key.pem" #your website's public key
